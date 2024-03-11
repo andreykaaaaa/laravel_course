ec3e274a0a8c672b90d41c87399f67eda9f78697
Андрей Рыжинский <ryzhinskuy.09@mail.ru>
2024-03-11 12:30:35 +0300

    feat: add pagination project

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Phonebook;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+
+    public function index()
+    {
+//        $users = Phonebook::all();
+        $users = Phonebook::orderBy('name')->paginate(10);
+        return view('home', compact('users'));
+    }
+
+    public function search(Request $request) {
+        $s = $request->s;
+        $users = Phonebook::where('name', 'LIKE', "%{$s}%")->orderBy('name')->paginate(10);
+        return view('home', compact('users'));
+    }
+}
